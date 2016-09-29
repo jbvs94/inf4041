cb9e7051f558af973533d047279e7976c93a1297
BARRON Jérémy <ETD-P\barron@i22-09-lx.etd-p.esiea.fr>
2016-09-29 16:16:40 +0200

    utilisation de beer_geek et des notes

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -69,6 +69,6 @@ class BeersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def beer_params
-      params.require(:beer).permit(:name, :description, :note, :brewery_id)
+      params.require(:beer).permit(:name, :description, :brewery_id, notes_attributes: [:star, :beer_geek_id, :id])
     end
 end
--- app/models/beer.rb
+++ app/models/beer.rb
@@ -2,4 +2,9 @@ class Beer < ActiveRecord::Base
 	belongs_to :brewery
 	has_many :notes
 	has_many :beer_geeks, through: :notes
+	accepts_nested_attributes_for :notes, allow_destroy: true
+
+	def moyenne
+			notes.map { |note| note.star }.sum / notes.count unless notes.empty?
+	end
 end
